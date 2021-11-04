938c5eab6a445a19641e71dbfd03f85c1afb9a13
Sobhan Gilani <Gilani.sobha@gmail.com>
2021-11-04 16:55:12 +0100

    Yes

--- cypress/integration/hello-world.spec.js
+++ cypress/integration/hello-world.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 
+
 // test suite
 
 describe('test suit example', function(){
@@ -10,11 +11,10 @@ describe('test suit example', function(){
       cy.contains('Affordable Professional Web Design')
       cy.get(':nth-child(2) > a').click()  
       cy.contains('About Us')
-      cy.get(':nth-child(3) > a')     
-        
-
-
-
+      cy.get(':nth-child(3) > a').click()     
+      cy.contains('Services') 
+      cy.get('form > input').type('mashti10@hotmail.com')
+      cy.get('.container > form > .button_1').click()
 
 
     })
